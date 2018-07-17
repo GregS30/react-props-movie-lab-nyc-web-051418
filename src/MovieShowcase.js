a253a9557086fb31a7591ae635605809fa52ff99
GregS30 <gjschmitt@live.com>
2018-07-17 08:36:35 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
+import uuid from 'uuid'
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    return movieData.map((movie) =>
+      <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genre={movie.genres}  poster={movie.poster} key={uuid()} />
+    );
   }
 
   render() {
